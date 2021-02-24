c6055e253d2d48c35fe5ed804135399689d7c557
lchhung@gmail.com <Hung55555>
2021-02-24 13:57:18 +0000

    add user role

--- src/main/java/com/lchhung/jwt/server/SecurityConfiguration.java
+++ src/main/java/com/lchhung/jwt/server/SecurityConfiguration.java
@@ -1,10 +1,21 @@
 package com.lchhung.jwt.server;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    // Need to override user role
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("ADMIN")
+                .and()
+                .withUser("user1").password("user1").roles("USER");
+    }
 }
