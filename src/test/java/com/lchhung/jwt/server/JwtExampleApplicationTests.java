1370b0a0e5bb2d72e4110b1bd9b008ea3d59d847
lchhung@gmail.com <Hung55555>
2021-02-24 13:52:57 +0000

    first commit

---
+++ src/test/java/com/lchhung/jwt/server/JwtExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lchhung.jwt.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
