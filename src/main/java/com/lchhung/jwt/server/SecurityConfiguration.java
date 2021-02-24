99b0b4a4ef7e3b36272422b59eb2d604f22484b5
lchhung@gmail.com <Hung55555>
2021-02-24 14:19:10 +0000

    add  password encoder

--- src/main/java/com/lchhung/jwt/server/SecurityConfiguration.java
+++ src/main/java/com/lchhung/jwt/server/SecurityConfiguration.java
@@ -1,21 +1,42 @@
 package com.lchhung.jwt.server;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    // Need to override user role
-
+    //1. Need to override user role
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("admin").password("admin").roles("ADMIN")
+                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN")
                 .and()
-                .withUser("user1").password("user1").roles("USER");
+                .withUser("user1").password(passwordEncoder().encode("user1")).roles("USER");
+    }
+
+    //2. Authorise request
+    @Override
+    protected void configure (HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests() // Authorise request
+                .anyRequest() // all request will be authorise
+                .authenticated()// Allow access to authenticated user only
+                .and()
+                .httpBasic();
+    }
+
+    //3. Need to provide a password encoder to hash password to be stored in plain text
+    @Bean
+    PasswordEncoder passwordEncoder (){
+        return new BCryptPasswordEncoder();
     }
 }
